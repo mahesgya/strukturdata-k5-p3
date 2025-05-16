b7e6cbaaf4ad7cc5cdfd1f23835a12e40e654784
unknown <anargya115@gmail.com>
2025-05-16 19:29:18 +0700

    feat: add loadReservationsFromCsv

--- src/reservation/reservations.cpp
+++ src/reservation/reservations.cpp
@@ -17,3 +17,43 @@ void ReservationManager::getReservationNextId() {
         }
     }
 }
+
+
+bool ReservationManager::loadReservationsFromCSV() {
+    std::ifstream file(filename);
+    
+    if (!file.is_open()) {
+        std::cerr << "Error: Tidak dapat membuka file " << filename << " untuk membaca data" << std::endl;
+        return false;
+    }
+    
+    reservations.clear();
+    std::string line;
+    
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string idStr, roomIdStr, userId, tanggalCheckIn, tanggalCheckOut, totalHargaStr, status;
+        
+        std::getline(ss, idStr, ',');
+        std::getline(ss, roomIdStr, ',');
+        std::getline(ss, userId, ',');
+        std::getline(ss, tanggalCheckIn, ',');
+        std::getline(ss, tanggalCheckOut, ',');
+        std::getline(ss, totalHargaStr, ',');
+        std::getline(ss, status, ',');
+        
+        Reservation reservation;
+        reservation.id = std::stoi(idStr);
+        reservation.roomId = std::stoi(roomIdStr);
+        reservation.userId = userId;
+        reservation.tanggalCheckIn = tanggalCheckIn;
+        reservation.tanggalCheckOut= tanggalCheckOut;
+        reservation.totalHarga = std::stod(totalHargaStr);
+        reservation.status = status;
+        
+        reservations.push_back(reservation);
+    }
+    
+    file.close();
+    return true;
+}
